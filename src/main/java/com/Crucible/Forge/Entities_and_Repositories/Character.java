52f16435f24fe9c7b5d39f057a36ba1298703e05
Chutton7743 <codeyhutton12@gmail.com>
2022-11-08 18:23:09 -0800

    rebuilt apis

--- src/main/java/com/Crucible/Forge/Entities_and_Repositories/Character.java
+++ src/main/java/com/Crucible/Forge/Entities_and_Repositories/Character.java
@@ -41,10 +41,4 @@ public class Character extends SubStats {
     private int speed;
     @Column (name = "size")
     private Size size;
-    public static String capitalize(String str) {
-        if (str == null || str.isEmpty()) {
-            return str;
-        }
-        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
-    }
 }
